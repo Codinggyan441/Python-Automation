22f1545b0651f06fdf465e49d086963c58140861
Codinggya441 <codinggyan4411@gmail.com>
2026-01-05 23:18:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,121 @@
+import pyautogui as s
+import time 
+
+s.press("win")
+time.sleep(1)
+
+s.write("notepad", interval=0.2)
+s.press("enter")
+
+time.sleep(1)
+
+s.hotkey("ctrl", "shift", "n")
+time.sleep(1)
+
+s.write("Going out tonight", interval=0.1)
+s.press("enter")
+
+s.write("Changes into something red", interval=0.1)
+s.press("enter")
+
+s.write("Her mother doesn't like that kind of dress", interval=0.1)
+s.press("enter")
+
+s.write("Everything she never had, she's showing off", interval=0.1)
+s.press("enter")
+
+s.write("Driving too fast", interval=0.1)
+s.press("enter")
+
+s.write("Moon is breaking through her hair", interval=0.1)
+s.press("enter")
+
+s.write("She's heading for something that she won't forget", interval=0.1)
+s.press("enter")
+
+s.write("Having no regrets is all that she really wants", interval=0.1)
+s.press("enter")
+
+s.write("We're only getting older, baby", interval=0.1)
+s.press("enter")
+
+s.write("And I've been thinking about it lately", interval=0.1)
+s.press("enter")
+
+s.write("Does it ever drive you crazy", interval=0.1)
+s.press("enter")
+
+s.write("Just how fast the night changes?", interval=0.1)
+s.press("enter")
+
+s.write("Everything that you've ever dreamed of", interval=0.1)
+s.press("enter")
+
+s.write("Disappearing when you wake up", interval=0.1)
+s.press("enter")
+
+s.write("But there's nothing to be afraid of", interval=0.1)
+s.press("enter")
+
+s.write("Even when the night changes", interval=0.1)
+s.press("enter")
+
+s.write("Even when the night changes", interval=0.1)
+s.press("enter")
+
+s.write("Even when the night changes", interval=0.1)
+s.press("enter")
+
+s.write("Chasing her tonight", interval=0.1)
+s.press("enter")
+
+s.write("Doubts are running 'round her head", interval=0.1)
+s.press("enter")
+
+s.write("He's waiting, hides behind a cigarette", interval=0.1)
+s.press("enter")
+
+s.write("Heart is beating loud and she doesn't want it to stop", interval=0.1)
+s.press("enter")
+
+s.write("Driving too fast", interval=0.1)
+s.press("enter")
+
+s.write("Moon is lighting up her skin", interval=0.1)
+s.press("enter")
+
+s.write("She's falling, doesn't even know it yet", interval=0.1)
+s.press("enter")
+
+s.write("Having no regrets is all that she really wants", interval=0.1)
+s.press("enter")
+
+s.write("We're only getting older, baby", interval=0.1)
+s.press("enter")
+
+s.write("And I've been thinking about it lately", interval=0.1)
+s.press("enter")
+
+s.write("Does it ever drive you crazy", interval=0.1)
+s.press("enter")
+
+s.write("Just how fast the night changes?", interval=0.1)
+s.press("enter")
+
+s.write("Everything that you've ever dreamed of", interval=0.1)
+s.press("enter")
+
+s.write("Disappearing when you wake up", interval=0.1)
+s.press("enter")
+
+s.write("But there's nothing to be afraid of", interval=0.1)
+s.press("enter")
+
+s.write("Even when the night changes", interval=0.1)
+s.press("enter")
+
+s.write("Even when the night changes", interval=0.1)
+s.press("enter")
+
+s.write("Even when the night changes", interval=0.1)
+s.press("enter")
